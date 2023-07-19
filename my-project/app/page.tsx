bfaaa88e8d9affa94d5a717763ca93f625780e78
kamran-hassan <hassan.kamran107@gmail.com>
2023-07-19 06:19:40 +0530

    color

---
+++ my-project/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-green-600 text-white border-8">
+      Hello World
+    </h1>
+    <h1 className="text-yellow-300 border-8">
+      Hello World
+    </h1>
+    </div>
+  ) 
+}
