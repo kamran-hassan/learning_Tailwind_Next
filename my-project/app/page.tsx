9f235f174b9225d15a5c0757ae2db87ce88d00ba
kamran-hassan <hassan.kamran107@gmail.com>
2023-07-19 06:42:14 +0530

    spacing cutomization

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -12,6 +12,17 @@ export default function Home() {
     <h1 className="text-mygreen border-mygreen border-8">
       Hello World
     </h1>
+    <h1 className="text-xs text-mygreen border-mygreen border-8 lg:text-lg sm:text-base">
+      Hello World
+    </h1>
+
+    <h1 className='ml-10 mt-10'>
+      Hello this is a sentence !
+    </h1>
+
+    <h1 className='mx-5'>
+      Hello this is another sentence !
+    </h1>
     </div>
   ) 
 }
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '100px',
+      '6': '48px',
+    },
     extend: {
       colors: {
         mygreen: "#49e659",
